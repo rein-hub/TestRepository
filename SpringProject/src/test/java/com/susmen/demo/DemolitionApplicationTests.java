08f91293facd46b11c3541162ea87f6af6475904
Reinis <pupolins11@gmail.com>
2021-10-06 13:55:19 +0300

    Committing normal update no thing suspicos

---
+++ SpringProject/src/test/java/com/susmen/demo/DemolitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susmen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemolitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
